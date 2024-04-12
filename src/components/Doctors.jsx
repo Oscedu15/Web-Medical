ff58afb05a42d4e186cc912353d5d04f55fdcaf4
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-04-12 10:55:38 -0300

    Modificiacion src Doctors

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -9,32 +9,32 @@ const Doctors = () => {
 
   const data = [
     {
-      img: "./src/assets/img/doc1.jpg",
+      img: "../src/assets/img/doc1.jpg",
       name: "Dr. Serena Mitchell",
       specialties: "Orthopedic Surgeon",
     },
     {
-      img: "./src/assets/img/doc2.jpg",
+      img: "../src/assets/img/doc2.jpg",
       name: "Dr. Julian Bennett",
       specialties: "Cardiologist",
     },
     {
-      img: "./src/assets/img/doc3.jpg",
+      img: "../src/assets/img/doc3.jpg",
       name: "Dr. Camila Rodriguez",
       specialties: "Pediatrician",
     },
     {
-      img: "./src/assets/img/doc4.jpg",
+      img: "../src/assets/img/doc4.jpg",
       name: "Dr. Victor Nguyen",
       specialties: "Neurologist",
     },
     {
-      img: "./src/assets/img/doc5.jpg",
+      img: "../src/assets/img/doc5.jpg",
       name: "Dr. Ethan Carter",
       specialties: "Dermatologist",
     },
     {
-      img: "./src/assets/img/doc6.jpg",
+      img: "../src/assets/img/doc6.jpg",
       name: "Dr. Olivia Martinez",
       specialties: "Ophthalmologist",
     },
